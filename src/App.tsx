eb8ab3643f4e73476dca3d1d7a97e7921f3f4c6e
Johan Trieloff <johan@trieloff.net>
2024-03-12 18:39:56 +0100

    basic form

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Map from './components/Map';
-import ReportButton from './components/report-button/report-button';
+import ReportButton from './components/ReportButton/ReportButton';
+import ReportForm from './components/ReportForm/ReportForm';
 import './App.css';
 
 function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+
   return (
     <div className="App">
       <div id="map">
         <Map />
       </div>
-      <ReportButton />
+      {isModalOpen && <ReportForm />}
+      <ReportButton onClick={() => setIsModalOpen(!isModalOpen)} />
     </div>
   );
 }
---
+++ src/components/ReportForm/ReportForm.tsx
@@ -0,0 +1,23 @@
+import './ReportForm.css';
+
+const ReportForm: React.FC = () => {
+    return (
+        <div className='report-form-container'>
+            <h1>Neue Meldung</h1>
+            <form>
+                <div>
+                    <input type='text' id='station' name='station' placeholder='Station' />
+                </div>
+                <div>
+                    <input type='text' id='line' name='line' placeholder='Linie' />
+                </div>
+                <div>
+                    <input type='text' id='direction' name='direction' placeholder='Richtung' />
+                </div>
+            </form>
+
+        </div>
+    );
+}
+
+export default ReportForm;

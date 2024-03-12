59bfe19ea8b04b7b741903e6decaae4fe9a9a70a
David Brandes <brandes.1@campus.tu-berlin.de>
2024-03-12 18:17:36 +0100

    Adding markers api fetch

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,20 +1,23 @@
 import { MapContainer, TileLayer, Popup, Marker } from "react-leaflet";
 import { LatLngTuple } from "leaflet";
-
+import Markers from "./markers/Markers";
 import './Map.css'
 
 export default function Map() {
     const position: LatLngTuple = [52.520008,13.404954];
-
+    const alexanderplatz: LatLngTuple = [52.521992, 13.413244];
   return (
     
-    
+
         <MapContainer id="map" center={position} zoom={13} scrollWheelZoom={true}>
         <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution= '<a href="https://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a>'
+            url={`https://tile.jawg.io/jawg-streets/{z}/{x}/{y}{r}.png?access-token=${process.env.REACT_APP_JAWG_ACCESS_TOKEN}`}
+            accessToken='FFEQAFoSuGPsfDwcON88gVkX5vZQkR0VNRMkfAOBYE7hrX9c7beONi36E5BNYPHo'
         />
-        <Marker position={position}>
+        <Markers />
+        
+        <Marker position={alexanderplatz}>
             <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
             </Popup>

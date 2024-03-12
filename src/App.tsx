b52cb29e26c689c029789732ec9be8f1272bb402
Johan Trieloff <johan@trieloff.net>
2024-03-12 16:49:34 +0100

    basic button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+
 import Map from './components/Map';
+import ReportButton from './components/report-button/report-button';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <ReportButton />
       <Map />
     </div>
   );

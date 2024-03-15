d4680d0c7119405274cbfff1a1a39d0d36697e69
Johan Trieloff <johan@trieloff.net>
2024-03-15 20:31:09 +0000

    opens backdrop and styling fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Map from './components/Map/Map';
 import ReportButton from './components/ReportButton/ReportButton';
@@ -8,24 +8,31 @@ import Backdrop from './components/Backdrop/Backdrop';
 import './App.css';
 
 function App() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isReportFormOpen, setIsReportFormOpen] = useState(false);
   const [formSubmitted, setFormSubmitted] = useState(false);
 
   const handleFormSubmit = () => {
     setFormSubmitted(prevState => !prevState);
   }
 
+  const [isUtilFormOpen, setIsUtilFormOpen] = useState(false);
+
   return (
     <div className='App'>
       <div id='map'>
         <Map formSubmitted={formSubmitted} />
       </div>
-      <UtilButton onClick={() => setIsModalOpen(!isModalOpen)}/>
-      <ReportButton onClick={() => setIsModalOpen(!isModalOpen)} />
-      {isModalOpen && (
+      <UtilButton onClick={() => setIsUtilFormOpen(!isUtilFormOpen)}/>
+      {isUtilFormOpen && (
         <>
-          <Backdrop onClick={() => setIsModalOpen(false)} />
-          <ReportForm closeModal={() => setIsModalOpen(false)} onFormSubmit={handleFormSubmit} />
+          <Backdrop onClick={() => setIsUtilFormOpen(false)} />
+        </>
+      )}
+      <ReportButton onClick={() => setIsReportFormOpen(!isReportFormOpen)} />
+      {isReportFormOpen && (
+        <>
+          <Backdrop onClick={() => setIsReportFormOpen(false)} />
+          <ReportForm closeModal={() => setIsReportFormOpen(false)} onFormSubmit={handleFormSubmit} />
         </>
       )}
     </div>

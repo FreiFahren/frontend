76777b4cb30223bade7f9ed31e69d37d9c158f86
Johan_Trieloff <johan@trieloff.net>
2024-03-17 12:38:19 +0100

    Add react-router-dom and update styles

--- src/components/UtilModal/UtilModal.tsx
+++ src/components/UtilModal/UtilModal.tsx
@@ -1,9 +1,16 @@
+import { Link } from 'react-router-dom';
+
 import './UtilModal.css';
 
 const UtilModal = () => {
     return (
         <div className='util-modal'>
-
+            <h1>Info</h1>
+            <button>Gib uns Feedback!</button>
+            <ul>
+                <li><Link to='/impressum'>Impressum</Link></li>
+                <li><Link to='/Datenschutz'>Datenschutz</Link></li>
+            </ul>
         </div>
     );
 }

1a3c931be8c7a4de6b4821f55726fec7a122d8af
Johan Trieloff <johan@trieloff.net>
2024-03-17 14:27:23 +0100

    util modal styling

--- src/components/UtilModal/UtilModal.tsx
+++ src/components/UtilModal/UtilModal.tsx
@@ -6,7 +6,7 @@ const UtilModal = () => {
     return (
         <div className='util-modal'>
             <h1>Info</h1>
-            <button>Gib uns Feedback!</button>
+            <button>💡 Gib uns Feedback!</button>
             <ul>
                 <li><Link to='/impressum'>Impressum</Link></li>
                 <li><Link to='/Datenschutz'>Datenschutz</Link></li>

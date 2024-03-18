309606b71d624a5e5f7ea9c4e075c7d32720cded
David Brandes <brandes.1@campus.tu-berlin.de>
2024-03-18 20:53:14 +0100

    removed unnecessary marker

--- src/functions/mapUtils.tsx
+++ src/functions/mapUtils.tsx
@@ -10,16 +10,6 @@ export const createLocationMarkerHTML = () => {
                 "></div>`;
     };
 
-export const createMarkerHTML = () => {
-        return `<div style="
-                    background-color:red;
-                    width:20px;
-                    height:20px;
-                    border-radius:50%;
-                    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.5);
-                    "></div>`;
-        };
-
 export const OpacityMarkerIcon = (opacity: number) => {
     const icon = L.divIcon({
         className: 'custom-icon',

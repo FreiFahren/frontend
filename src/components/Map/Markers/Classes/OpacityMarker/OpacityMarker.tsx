64c6861ee47bb9739fb1c793a0e4fc12e947f245
Johan_Trieloff <johan@trieloff.net>
2024-03-21 14:48:40 +0100

    clean up

--- src/components/Map/Markers/Classes/OpacityMarker/OpacityMarker.tsx
+++ src/components/Map/Markers/Classes/OpacityMarker/OpacityMarker.tsx
@@ -2,62 +2,57 @@ import { Marker, Popup } from 'react-leaflet';
 import { MarkerData } from '../../MarkerContainer';
 import { OpacityMarkerIcon } from '../../../../../functions/mapUtils';
 import L from 'leaflet';
-import { useRef, useEffect, useCallback, useState } from 'react';
+import { useRef, useEffect, useState } from 'react';
 
 export const OpacityMarker = ({ markerData, index }: { markerData: MarkerData; index: number; }) => {
-    const [opacity, setOpacity] = useState(0);
+    const [opacity, setOpacity] = useState(1); // full opacity so that the component renders initially
     const { timestamp, station, line, direction } = markerData;
 
     const timestampSeconds = new Date(timestamp.replace(/T|Z/g, ' ')).getTime();
 
-    const markerRef = useRef<L.Marker | null>(null); // Create a ref to store the marker instance
-
-    const calculateOpacity = useCallback(() => { // Wrapped in useCallback
-        const currentTime = new Date().getTime();
-        const elapsedTime = currentTime - timestampSeconds;
-        const opacityValue = Math.max(0, 1 - (elapsedTime / (15 * 60 * 1000)));
-        setOpacity(opacityValue);
-        return opacityValue;
-    }, [timestampSeconds]); // timestampSeconds is a dependency
+    const markerRef = useRef<L.Marker | null>(null);
 
     useEffect(() => {
-        calculateOpacity();
-
-        const interval = setInterval(() => {
-            calculateOpacity();
-
-            if (opacity === 0) {
-                clearInterval(interval);
+        // ensures that intervalId is defined before it's used
+        let intervalId: NodeJS.Timeout;
+        
+        const calculateOpacity = () => {
+            const currentTime = new Date().getTime();
+            const elapsedTime = currentTime - timestampSeconds;
+            const newOpacity = Math.max(0, 1 - (elapsedTime / (15 * 60 * 1000)));
+            setOpacity(newOpacity);
+            if (newOpacity === 0) {
+                clearInterval(intervalId);
             }
-        }, 3000);
-
-        return () => {
-            clearInterval(interval);
         };
-    }, [calculateOpacity, opacity]); // calculateOpacity is now a stable function
+
+        calculateOpacity(); // Initial calculation
+
+        intervalId = setInterval(calculateOpacity, 5000); // every 5 seconds to avoid excessive rerenders
+
+        return () => clearInterval(intervalId);
+    }, [timestampSeconds]); // runs when getting a new timestamp
 
     useEffect(() => {
-        if (markerRef.current) {
+        if (markerRef.current && opacity > 0) {
             const newIcon = OpacityMarkerIcon(opacity);
-            markerRef.current.setIcon(newIcon); // Update the icon of the marker instance
+            markerRef.current.setIcon(newIcon);
         }
-    }, [opacity]); // Run this effect whenever the opacity changes
+    }, [opacity]);
 
-    if (opacity === 0) {
+    if (opacity <= 0) {
         return null;
     }
 
     return (
         <Marker 
-            ref={markerRef} // Pass the ref to the Marker component
-            key={`${line}-${index}`} 
+            ref={markerRef}
+            key={`${line}-${index}`}
             position={[station.coordinates.latitude, station.coordinates.longitude]} 
             icon={OpacityMarkerIcon(opacity)}
         >
             <Popup>
-                <>
-                    {line} {direction.name ? direction.name + ' - ' : ''} {station.name} {opacity.toFixed(2)}
-                </>
+                {`${line} ${direction.name ? direction.name + ' - ' : ''} ${station.name}`}
             </Popup>
         </Marker>
     );

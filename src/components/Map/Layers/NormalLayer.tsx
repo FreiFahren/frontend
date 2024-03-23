13920e3f5ffba1b8d1bd61fc911ed8a60fc40c08
David Brandes <brandes.1@campus.tu-berlin.de>
2024-03-23 18:01:59 +0100

    added some controls

--- src/components/Map/Layers/NormalLayer.tsx
+++ src/components/Map/Layers/NormalLayer.tsx
@@ -1,16 +1,22 @@
 import { TileLayer, useMapEvents } from 'react-leaflet';
-
-
+import L from 'leaflet';
 import { urlTemplate } from '../Map';
 
 const NormalLayer = () => {
     const map = useMapEvents({
-
+        click: (e) => {
+            console.log('Clicked location:', e.latlng);
+        }
     });
 
     map.setMaxZoom(17);
     map.setMinZoom(11);
-
+    
+    const layer = new L.TileLayer(urlTemplate, {
+        maxZoom: 17,
+        minZoom: 11,
+        attribution: '<a href="https://www.jawg.io?utm_medium=map&utm_source=attribution" target="_blank">&copy; Jawg</a> - <a href="https://www.openstreetmap.org?utm_medium=map-attribution&utm_source=jawg" target="_blank">&copy; OpenStreetMap</a>&nbsp;contributors'
+    }).addTo(map);
     
     return (
         <TileLayer
--- src/components/Map/Layers/OfflineLayer.tsx
+++ src/components/Map/Layers/OfflineLayer.tsx
@@ -1,22 +1,21 @@
 
 import TileManager from 'leaflet.offline' ;
 
-import { urlTemplateDark } from '../Map';
+import { urlTemplate } from '../Map';
 import { useMapEvents } from 'react-leaflet';
 
 const OfflineLayer = () => {
     const map = useMapEvents({
         zoomend: () => {
             console.log('Zoom level', map.getZoom());
-            
-            saveControl();
+            console.log(saveControl().status.storagesize);
             console.log('Map loaded');
             
         }
     })
 
     
-     const layerOffline = TileManager.tileLayerOffline(urlTemplateDark, {
+     const layerOffline = TileManager.tileLayerOffline(urlTemplate, {
         attribution: '<a href="https://www.jawg.io?utm_medium=map&utm_source=attribution" target="_blank">&copy; Jawg</a> - <a href="https://www.openstreetmap.org?utm_medium=map-attribution&utm_source=jawg" target="_blank">&copy; OpenStreetMap</a>&nbsp;contributors',
         maxZoom: 17,
         minZoom: 11,
@@ -28,8 +27,9 @@ const OfflineLayer = () => {
         zoomlevels: [13, 16], // optional zoomlevels to save, default current zoomlevel
         alwaysDownload: false,
         parallel: 4,
-        saveWhatYouSee: false,
+        saveWhatYouSee: true,
       });
+
 
      
     

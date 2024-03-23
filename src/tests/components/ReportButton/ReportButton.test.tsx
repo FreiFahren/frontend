a6d80342acff335fa908bec2424350eeeb72e6ad
Johan Trieloff <johan@trieloff.net>
2024-03-23 15:40:52 +0100

    ReportForm highlight test

--- src/tests/components/ReportButton/ReportButton.test.tsx
+++ src/tests/components/ReportButton/ReportButton.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
+
 import ReportButton from '../../../components/ReportButton/ReportButton';
 
 describe('ReportButton', () => {

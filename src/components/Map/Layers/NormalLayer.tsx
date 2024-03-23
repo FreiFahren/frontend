1209261a91f00d9221b5fced86d474de17a02065
David Brandes <brandes.1@campus.tu-berlin.de>
2024-03-23 18:33:07 +0100

    edited offline layer

--- src/components/Map/Layers/NormalLayer.tsx
+++ src/components/Map/Layers/NormalLayer.tsx
@@ -1,6 +1,6 @@
 import { TileLayer, useMapEvents } from 'react-leaflet';
-import L from 'leaflet';
 import { urlTemplate } from '../Map';
+import { tileLayer } from 'leaflet';
 
 const NormalLayer = () => {
     const map = useMapEvents({
@@ -9,20 +9,23 @@ const NormalLayer = () => {
         }
     });
 
+
     map.setMaxZoom(17);
     map.setMinZoom(11);
     
-    const layer = new L.TileLayer(urlTemplate, {
+    const layer = tileLayer(urlTemplate, {
         maxZoom: 17,
         minZoom: 11,
+
         attribution: '<a href="https://www.jawg.io?utm_medium=map&utm_source=attribution" target="_blank">&copy; Jawg</a> - <a href="https://www.openstreetmap.org?utm_medium=map-attribution&utm_source=jawg" target="_blank">&copy; OpenStreetMap</a>&nbsp;contributors'
-    }).addTo(map);
+    })
     
     return (
-        <TileLayer
-        attribution= '<a href=\"https://www.jawg.io?utm_medium=map&utm_source=attribution\" target=\"_blank\">&copy; Jawg</a> - <a href=\"https://www.openstreetmap.org?utm_medium=map-attribution&utm_source=jawg\" target=\"_blank\">&copy; OpenStreetMap</a>&nbsp;contributors'
-        url={urlTemplate}
-         />
+        <></>
+        // <TileLayer
+        // attribution= '<a href=\"https://www.jawg.io?utm_medium=map&utm_source=attribution\" target=\"_blank\">&copy; Jawg</a> - <a href=\"https://www.openstreetmap.org?utm_medium=map-attribution&utm_source=jawg\" target=\"_blank\">&copy; OpenStreetMap</a>&nbsp;contributors'
+        // url={urlTemplate}
+        //  />
     )
 }
 
--- src/components/Map/Layers/OfflineLayer.tsx
+++ src/components/Map/Layers/OfflineLayer.tsx
@@ -1,37 +1,27 @@
 
 import TileManager from 'leaflet.offline' ;
 
-import { urlTemplate } from '../Map';
+import { urlTemplate, urlTemplateDark } from '../Map';
 import { useMapEvents } from 'react-leaflet';
 
 const OfflineLayer = () => {
     const map = useMapEvents({
         zoomend: () => {
-            console.log('Zoom level', map.getZoom());
-            console.log(saveControl().status.storagesize);
-            console.log('Map loaded');
-            
+
         }
     })
 
     
-     const layerOffline = TileManager.tileLayerOffline(urlTemplate, {
+     const layerOffline = TileManager.tileLayerOffline(urlTemplateDark, {
         attribution: '<a href="https://www.jawg.io?utm_medium=map&utm_source=attribution" target="_blank">&copy; Jawg</a> - <a href="https://www.openstreetmap.org?utm_medium=map-attribution&utm_source=jawg" target="_blank">&copy; OpenStreetMap</a>&nbsp;contributors',
         maxZoom: 17,
         minZoom: 11,
 
     }).addTo(map);
 
-    
-    const saveControl = () => TileManager.savetiles(layerOffline, {
-        zoomlevels: [13, 16], // optional zoomlevels to save, default current zoomlevel
-        alwaysDownload: false,
-        parallel: 4,
-        saveWhatYouSee: true,
-      });
 
 
-     
+   
     
 
     layerOffline.on('savestart', (e) => {

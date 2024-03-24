5e9a5f4db09e950ed842e7dc43cec39f16991e17
David Brandes <brandes.1@campus.tu-berlin.de>
2024-03-24 12:08:03 +0100

    own maps

--- src/components/Map/Layers/StandardLayer.tsx
+++ src/components/Map/Layers/StandardLayer.tsx
@@ -1,16 +1,19 @@
-import { useMap } from "react-leaflet"
-import L from 'leaflet'
+import { useMap } from 'react-leaflet';
+import L from 'leaflet';
+import { useEffect } from 'react';
 
 const StandardLayer = () => {
-    const map = useMap()
-    map.setMaxZoom(15)
-    map.setMinZoom(12)
+  const map = useMap();
+
+  useEffect(() => {
+    map.setMaxZoom(15);
+    map.setMinZoom(12);
     L.tileLayer(`${process.env.PUBLIC_URL}/tiles/{z}/{x}/{y}.png`, {
-        keepBuffer: 4, // Specify the number of additional tile rows and columns to load outside the view
-        errorTileUrl: `${process.env.PUBLIC_URL}/tiles/{z}/{x}/{y}.png`, // URL for the low-resolution tile image
+      keepBuffer: 4, // Specify the number of additional tile rows and columns to load outside the view
     }).addTo(map);
+  }, []); // Empty dependency array means this effect runs once on mount
 
-    return null
-}
+  return null;
+};
 
-export default StandardLayer
+export default StandardLayer;
